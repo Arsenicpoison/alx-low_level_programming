4b454ef629e9b2ddc02c65e5030d5c4011157600
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-15 08:21:52 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print ten times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int letter, let;
+
+	for (let = 0; let < 10; let++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+}
