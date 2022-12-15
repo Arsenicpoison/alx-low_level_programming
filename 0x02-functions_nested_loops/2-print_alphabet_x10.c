a17634e3e1db2425ecd0dc8ae1b0762ed158dd3a
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-15 08:24:13 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,11 @@ void print_alphabet_x10(void)
 	int letter, let;
 
 	for (let = 0; let < 10; let++)
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
+		}
 		_putchar('\n');
+	}
 }
