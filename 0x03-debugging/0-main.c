138c08a3dbaebff33516d11afe095101479165cf
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-15 13:56:53 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test if a integer is a positive or negative
+ * @n:integer to ckeck
+ * Return: 0
+ */
+void main(int n)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+i}
