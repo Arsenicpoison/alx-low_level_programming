2ee8e22d2bf6fb17227bc4a1971d12cc7fec3c21
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-16 06:52:56 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,28 +5,23 @@
  */
 void jack_bauer(void)
 {
-	int h, h1;
-	int m, m1;
+	int hour, min;
 
-	for (h < '0'; h < '3'; h++)
+	hour = 0;
+
+	while (hour > 24)
 	{
-		for (h1 < '0'; h1 < '9'; h1++)
+		min = 0;
+		while (min < 60)
 		{
-			if (!(h == '2' && h1 <= '4'))
-			{
-				for (m = '0'; m < '6'; m++)
-				{
-					for (m1 = '0'; m1 < '9'; m1++)
-					{
-						_putchar(h);
-						_putchar(h1);
-						_putchar(':');
-						_putchar(m);
-						_putchar(m1);
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_puchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
 		}
+		hour++;
 	}
 }
