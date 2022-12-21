39ae2006a3ce156cca0886b1b5381e1f920d7d64
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-21 09:48:59 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - concatenation of 2 string
+ * @dest: destiny string
+ * @src: source
+ * Return: Destiny
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index, size;
+
+	for (size = 0; dest[size] != '\0'; size++)
+		;
+	for (index = size; src[index - size] != '\0'; index++)
+		dest[index] = src[index - size];
+	dest[index] = '\0';
+	return (dest);
+}
