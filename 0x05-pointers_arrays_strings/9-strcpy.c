bf3e8d9b86949a24094c1773af8fc2894b7abc6f
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-21 01:02:32 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - cpy a tsring in str desk
+ * @src:source
+ * @dest:destiny
+ * Return:copy char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+		dest[index] = src[index];
+	dest[index0] = '\0'
+	return (dest);
+}
+
