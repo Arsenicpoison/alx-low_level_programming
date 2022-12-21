bea89c40ea1e68d53382d17dbd6aa26a897ce64b
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-20 17:41:30 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts -print a string followed by a new line
+ * @str : pointer to a string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(20);
+}
