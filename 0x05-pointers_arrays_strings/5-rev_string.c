050a4163689c245f9184e7c7fbb6101b354d88fc
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-20 18:19:59 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s: pointer to a string
+ */
+void rev_string(char *s)
+{
+	int size, i, j;
+	char c;
+
+	for (size =  0; s[size] != '\0'; size++)
+		;
+	j = size - 1;
+	for (i = 0; j >= 0 && i < j; j--; i++)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
