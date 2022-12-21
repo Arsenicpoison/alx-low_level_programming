3b6237d767b5e5710eb1e672af0e274a8433994d
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-20 17:53:43 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: pointer to a string
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '0\'; length++);
+	for (length--; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar(10);
+}
