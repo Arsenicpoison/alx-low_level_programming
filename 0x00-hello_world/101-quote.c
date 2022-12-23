834f70192f0296bbdaa2ba7821449e976dc53688
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-23 08:24:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf(1, "and that piece of art is useful\" - Dora Korpar , 2015-10-19", 59);
+	printf("and that piece of art is useful\" - Dora Korpar , 2015-10-19");
 	return (1);
 }
