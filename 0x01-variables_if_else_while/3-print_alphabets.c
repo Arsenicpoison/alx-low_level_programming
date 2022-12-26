9f609f12d685d62074caeb0c5b101be5431a518a
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-26 06:53:11 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	chr leter_a, leter_z;
+
+	leter_a = 'a';
+	leter_z = 'z';
+
+	while (leter_a <= leter_z)
+	{
+		putchar(leter_a);
+		leter_a++;
+		if (leter_a == 123)
+		{
+			leter_a = 'A';
+			leter_z = 'Z';
+		}
+	}
+	putchar(10);
+	return (0);
+}
