3612b977f1ed045d3b9d90ac886d27883041728b
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-28 23:20:49 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char leter;
+
+
+	for (leter = 0; leter <= '9'; leter++)
+	{
+		putchar(leter);
+	}
+	putchar(10);
+	return (0);
+}
