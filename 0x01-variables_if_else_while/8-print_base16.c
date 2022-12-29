68abfb1ee3b0213684b12bcac44b4447f63d223f
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-29 00:36:31 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print base16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char base16;
+
+	for (base16 = '0'; base16 <= '9'; bse16++)
+	{
+		putchar(base16);
+	}
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
+		putchar(base16);
+	}
+	putchar(10);
+	return (0);
+}
