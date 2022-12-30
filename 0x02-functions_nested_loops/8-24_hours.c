26f08208803eb92b5768282c2b24c2c8b24865ac
Arsenicpoison <arseneirankunda7@gmail.com>
2022-12-29 22:47:10 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		while (min < 60)
 		{
 			_putchar((hour / 10) + '0');
-			_puchar((hour % 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
