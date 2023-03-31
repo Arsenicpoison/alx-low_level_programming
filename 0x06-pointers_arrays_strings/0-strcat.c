8749115356988da4d13a944dfe7c94384d306310
Arsenicpoison <arseneirankunda7@gmail.com>
2023-03-31 10:41:40 -0700

    0-strcat.c updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
- * _strcat - concatenation of 2 string
- * @dest: destiny string
- * @src: source
- * Return: Destiny string
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-	int index, size;
+	int lengthE, lengthD;
 
-	for (size = 0; dest[size] != '\0'; size++);
-	for (index = size; src[index - size] != '\0'; index++)
-		dest[index] = src[index - size];
-	dest[index] = '\0';
+	lengthE = 0;
+	lengthD = 0;
+
+	while (*(dest + lengthE) != '\0')
+		lengthE++;
+
+	while (*(src + lengthD) != '\0' && lengthE < 97)
+	{
+		*(dest + lengthE) = *(src + lengthD);
+		lengthE++;
+		lengthD++;
+	}
+	*(dest + lengthE) = '\0';
 	return (dest);
 }
