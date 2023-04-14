30614f15546e5cdfed14c812ccf233463d2edef4
Arsenicpoison <arseneirankunda7@gmail.com>
2023-04-14 15:44:30 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: arguments
+ * @argv: args
+ * Return: int
+**/
+int main(int argc, char *argv[])
+{
+	int count1, count2;
+	unsigned long mul;
+
+	mul = 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	while (count1 < argc)
+	{
+	while (argv[count1][count2] != '\0')
+	{
+		if (argv[count1][count2] > 57 || argv[count1][count2] < 48)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		count2++;
+	}
+		count1++;
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
