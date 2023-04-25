a247e2420c203982704ca319823185555664bcaf
Arsenicpoison <arseneirankunda7@gmail.com>
2023-04-25 16:16:50 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int count = 0;
 
 	va_start(list, n);
-	while (count < n)
+	for (; count < n; count++)
 	{
 		printf("%d", va_arg(list, int));
 		if (count < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-		count++;
 	}
 	va_end(count);
 	printf("\n");
