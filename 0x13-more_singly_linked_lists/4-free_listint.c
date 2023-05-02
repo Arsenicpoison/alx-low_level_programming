bc01e5d59d90112c46189d03446d8350a63d868b
Arsenicpoison <arseneirankunda7@gmail.com>
2023-05-02 08:43:42 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a listint_t list.
+ * @head: Head of linked listint_t list.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tail = NULL;
+
+	for (; head != NULL;)
+		tail = head->next;
+		free(head);
+		head = tail;
+}
